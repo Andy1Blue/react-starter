31ef1283df01f30ec9279abaf8dead6fbca49e2d
Krzysztof Bujak <bujak.krzysztof@outlook.com>
2020-11-30 08:33:22 +0100

    Fix test

--- test/Browser/TestCases/Components/App/App.spec.js
+++ test/Browser/TestCases/Components/App/App.spec.js
@@ -3,12 +3,13 @@ describe('App component tests', () => {
     cy.visit('/');
   });
 
-  it('should display title and paragraph', () => {
-    cy.get('h1').contains("It's React Starter from React! We love TypeScript!");
+  it('should display title and subtitle', () => {
+    cy.get('.app .app__title').contains('Hello 👋');
+    cy.get('.app .app__subtitle').contains("It's your React Starter!");
   });
 
   it('should display app version in red color', () => {
-    cy.get('.app-version').contains('App version: 1.0');
-    cy.get('.app-version').should('have.css', 'color', 'rgb(255, 0, 0)');
+    cy.get('.app .app__version--red').contains('App version: 1.0');
+    cy.get('.app .app__version--red').should('have.css', 'color', 'rgb(255, 0, 0)');
   });
 });
