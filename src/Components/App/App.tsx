17d53c8cd46f2595624562de224b65fc72238a49
Krzysztof Bujak <bujak.krzysztof@outlook.com>
2020-11-30 08:27:22 +0100

    Modify design

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -3,13 +3,23 @@ import './App.scss';
 
 export interface Props {
   appName: string;
-  appLang: string;
 }
 
 export const App = (props: Props) => (
-  <div>
-    <h1>Welcome to {props.appName}!</h1>
-    <h2>We love {props.appLang}!</h2>
-    <p className="app-version">App version: {process.env.APP_VER}</p>
+  <div className="app">
+    <h1 className="app__title">Hello &#128075;</h1>
+    <h2 className="app__subtitle">It's your {props.appName}!</h2>
+    <p className="app__version app__version--red">App version: {process.env.APP_VER}</p>
+    <p>
+      <a
+        className="github-button"
+        href="https://github.com/Andy1Blue/react-star"
+        data-icon="octicon-star"
+        data-size="large"
+        aria-label="Star Andy1Blue/react-star on GitHub"
+      >
+        Star
+      </a>
+    </p>
   </div>
 );
--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,4 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { App } from './Components/App/App';
 
-ReactDOM.render(<App appName="React Starter" appLang="TypeScript" />, document.getElementById('root'));
+ReactDOM.render(<App appName="React Starter" />, document.getElementById('root'));
